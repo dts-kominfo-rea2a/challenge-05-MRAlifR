d296efcae3ae5b8a804aef443e88df8518499ebb
M Rizki Alif R <mrizkialifr@gmail.com>
2022-06-04 20:27:48 +0700

    Update sorter function

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (nameArray, fnSorter) => {
+  const sortedNames = fnSorter(nameArray);
+  const formattedNames = [];
+  for (let index = 0; index < sortedNames.length; index++) {
+    const name = sortedNames[index];
+    formattedNames.push((index+1) + ". " + name);
+  }
+  return formattedNames;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (nameArray) => nameArray.sort();
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (nameArray) => nameArray.sort().reverse();
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
